e433045ec7d7a7595de287e85cffacf3ed339aff
yoshiomori <yoshiomori90@gmail.com>
2015-06-06 23:54:36 -0300

    Implementação
    Lidar com a mudança do tamanho da tela.

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+var canvas;
+var gl;
+
+function setUpScreen(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+}
+
 function main(){
-	var canvas = document.getElementById("screen");
+	canvas = document.getElementById("screen");
 	if (!canvas) {
 		console.log("Failed to retrieve the <canvas> element");
 		return;
 	}
-	canvas.width = window.outerWidth;
-	canvas.height = window.outerHeight;
-	var webGL = canvas.getContext("webgl");
-	webGL.clearColor(0.0, 0.0, 0.0, 1.0);
-	webGL.clear(webGL.COLOR_BUFFER_BIT);
+	gl = canvas.getContext("webgl");
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	if (!gl){
+		console.log("Failed to get the rendering context for WebGL");
+		return;
+	}
+	setUpScreen();
+	gl.clear(gl.COLOR_BUFFER_BIT);
 };
