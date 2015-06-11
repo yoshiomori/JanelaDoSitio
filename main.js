480439015d2927f9818e92b4158646a71c4cc9ce
yoshiomori <yoshiomori90@gmail.com>
2015-06-11 13:51:11 -0300

    Implementação
    Texturas

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function initShader(type, source, program){
 
 function render(){
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.POINTS, 0, 1);
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
     window.requestAnimationFrame(render);
 }
 
@@ -31,7 +31,6 @@ function main(){
 		return;
 	}
 	gl = canvas.getContext("webgl");
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	if (!gl){
 		console.log("Failed to get the rendering context for WebGL");
 		return;
@@ -40,13 +39,21 @@ function main(){
 	
 	// Programa shader
 	var VSHADER_SOURCE =
-	"void main(){\n"+
-	" gl_Position = vec4(0,0,0,1);\n"+
-	" gl_PointSize = 10.0;\n"+
+	"attribute vec4 a_Position;\n" +
+	"attribute vec2 a_TexCoord;\n" +
+	"varying vec2 v_TexCoord;\n" +
+	"void main(){\n" +
+	" gl_Position = a_Position;\n" +
+	" v_TexCoord = a_TexCoord;\n" +
 	"}\n";
 	var FSHADER_SOURCE =
-	"void main(){\n"+
-	" gl_FragColor = vec4(1,0,0,1);\n"+
+	"#ifdef GL_ES\n" +
+	"precision mediump float;\n" +
+	"#endif\n" +
+	"uniform sampler2D u_Sampler;\n" +
+	"varying vec2 v_TexCoord;\n" +
+	"void main(){\n" +
+	" gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n" +
 	"}\n";
 	var program = gl.createProgram();
 	var vertexShader = initShader(gl.VERTEX_SHADER, VSHADER_SOURCE, program);
@@ -62,5 +69,65 @@ function main(){
 		return null;
 	}
 	gl.useProgram(program);
-	render();
+
+	// Carregando dados dos vértices no programa
+	var y = Math.sqrt(1 - 0.5 * 0.5) / 2;
+	var verticesTexCoords = new Float32Array([
+	 0.0,  y,   0.5, 0.5 + y,
+	-0.5, -y,   0.0, 0.5 - y,
+	 0.5, -y,   1.0, 0.5 - y
+	]);
+	var n = 3;
+	var vertexBuffer = gl.createBuffer();
+	if (!vertexBuffer) {
+		console.log("Failed to create the buffer object");
+		return -1;
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
+	var FSIZE = verticesTexCoords.BYTES_PER_ELEMENT;
+	var a_Position = gl.getAttribLocation(program, "a_Position");
+	if (a_Position < 0) {
+		console.log("Failed to get the storage location of a_Position");
+		return -1;
+	}
+	gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
+	gl.enableVertexAttribArray(a_Position);
+	var a_TexCoord = gl.getAttribLocation(program, "a_TexCoord");
+	if (a_TexCoord < 0) {
+		console.log("FAiled to get the storage location of a_TexCoord");
+		return -1;
+	}
+	gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+	gl.enableVertexAttribArray(a_TexCoord);
+	
+	// Iniciando texturas
+	var texture = gl.createTexture();
+	if (!texture) {
+		console.log("Failed to create the texture object");
+		return false;
+	}
+	var u_Sampler = gl.getUniformLocation(program, "u_Sampler");
+	if (!u_Sampler) {
+		console.log("Failed to get the storege location of u_Sampler");
+		return false;
+	}
+	var image = new Image();
+	if (!image) {
+		console.log("Failed to create the image object");
+		return false;
+	}
+	image.onload = function(){loadTexture(gl, n, texture, u_Sampler, image);};
+	image.src = "sky.jpg";
+	//render(n);
+}
+
+function loadTexture (gl, n, texture, u_Sampler, image) {
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+	gl.uniform1i(u_Sampler, 0);
+	render(n);
 }
